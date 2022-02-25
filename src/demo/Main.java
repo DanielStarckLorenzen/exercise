f568175f8c4b1c78155e47e207dd5707d0cb9214
daniellorenzen <dani141@stud.kea.dk>
2022-02-25 09:22:09 +0100

    added greet

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -1,8 +1,21 @@
 package demo;
 
+import java.util.Scanner;
+
 public class Main {
+
+    Scanner in = new Scanner(System.in);
+
+    void greet() {
+        System.out.print("Enter name: ");
+        String name = in.nextLine();
+        System.out.println("Hello " + name);
+    }
 
     public static void main(String[] args) {
         System.out.println("It works!");
+
+        new Main().greet();
+
     }
 }
